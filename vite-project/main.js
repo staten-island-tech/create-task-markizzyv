fac93ec5c4fa1a3bf83e38f77b380254c7883617
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-01-16 09:15:55 -0500

    f
    f

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,21 +4,82 @@ import viteLogo from '/vite.svg'
 
 // const URL = `https://api.themoviedb.org/3/movie/popular?api_key=26906062d4fd4de4f857063554f6f6d3&page=1`;  
 
+const URL = `https://imdb-api.com/en/API/Search/k_12345678/inception 2010`
+
+async function getData(URL){
+  try {
+    const response = await fetch(URL);
+    if (response.status !=200){
+      //let pokemon = peekachew
+      // throw new Error(pokemon);
+    }
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+     //document.querySelector("h1").textContent = `Sorry I could not find peekachew ${pokemon}`;
+  }
+}
+getData(URL);
+
+
+/* const URL = `https://api.themoviedb.org/3/movie/popular?api_key=26906062d4fd4de4f857063554f6f6d3&page=1`;
+
+//insert display cards
+function movInsert(arr) {
+
+
+  document.querySelector(".flex-container").innerHTML = '';
+  
+  
+      arr.forEach((card)=> {
+        const {title, poster_path, vote_average} = card;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = 
+       
+      ` <div class= "card">
+      <h1 class ="movTitle">${title}</h1>  
+      <img src = "https://image.tmdb.org/t/p/w500${poster_path}" class ="movImg" alt = "">
+      <p class ="movVoteAverage">${vote_average}</p>
+      </div>
+    `
+      document.querySelector(".flex-container").appendChild(movieEl);
+  
+
+  });
+}
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status !=200) {
+      throw new Error(response.statusText);
+    }
+    console.log(response);
+    const data = await response.json();
+    console.log(data)
+    movInsert(data.results); 
+   
+    if (data.results.length === 0) {
+      document.querySelector(".error").textContent = "No Results Found: try a good movie instead buddy";
+      document.querySelector(".flex-container").innerHTML = "";
+      return data
+    }
+  } catch (error) {
+    document.querySelector(".error").textContent = "Error Data Cannot Be Found";
+  }
+}
+
+
+getData(URL)
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
+ */
 
 
 
