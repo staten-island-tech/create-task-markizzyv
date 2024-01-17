323ccdc1c59fe21814571fbd9526ea4fe1c1f1a6
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-01-17 08:53:45 -0500

    d
    d

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,20 +4,19 @@ import viteLogo from '/vite.svg'
 
 // const URL = `https://api.themoviedb.org/3/movie/popular?api_key=26906062d4fd4de4f857063554f6f6d3&page=1`;  
 
-const URL = `https://imdb-api.com/en/API/Search/k_12345678/inception 2010`
+const URL = `https://imdb-api.com/en/API/Top250Movies/k_12345678`
 
 async function getData(URL){
   try {
     const response = await fetch(URL);
     if (response.status !=200){
-      //let pokemon = peekachew
-      // throw new Error(pokemon);
+     
     }
     console.log(response);
     const data = await response.json();
     console.log(data);
   } catch (error) {
-     //document.querySelector("h1").textContent = `Sorry I could not find peekachew ${pokemon}`;
+  
   }
 }
 getData(URL);
